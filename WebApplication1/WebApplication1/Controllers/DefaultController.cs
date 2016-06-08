d52ef2b8bf70b39001270a5017698f423fae4bc4
snagannagari <snagannagari@csc.com>
2016-06-08 12:46:41 +0530

    commit1

--- WebApplication1/WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -13,5 +13,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult NewIndex()
+        {
+            return View();
+        }
     }
 }
